61a0bd52142e28d81d91f3c1113561b611011feb
ShenNya <ntgtech20060605@gmail.com>
2024-07-26 22:06:11 +0800

    Sync Code, Fix Scroll bug, Change material Icon into fluent Icon

--- Netko/NetDisk/Baidu/BaiduShare.cs
+++ Netko/NetDisk/Baidu/BaiduShare.cs
@@ -14,10 +14,17 @@ namespace Netko.NetDisk.Baidu
     {
         public Baidu Account = account;
 
+        public const string app_id = "250528";
+        public const string channel = "chunlei";
+        public const string clienttype = "0";
+        public const string netdisk_user_agent = "netdisk";
+        public const string broswer_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52";
+
         public JsonArray get_share_list(string shareid, string uk, string randsk)
         {
-            string url = $"https://pan.baidu.com/share/list?app_id={Account.app_id}&channel={channel}&clienttype=0&desc=1&num=100&order=time&page=1&root=1&shareid={shareid}&showempty=0&uk={uk}&web=1";
-            return;
+            string url = $"https://pan.baidu.com/share/list?app_id={app_id}&channel={channel}&clienttype=0&desc=1&num=100&order=time&page=1&root=1&shareid={shareid}&showempty=0&uk={uk}&web=1";
+            JsonArray sharelist = new JsonArray();
+            return sharelist;
         }
     }
 }
